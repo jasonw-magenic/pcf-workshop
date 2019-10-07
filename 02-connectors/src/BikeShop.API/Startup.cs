058888a90c6ea91209405b70de631fdb1c5ec60d
jasonw-magenic <jasonw@magenic.com>
2019-10-07 01:27:37 -0400

    Update lab #2

--- 02-connectors/src/BikeShop.API/Startup.cs
+++ 02-connectors/src/BikeShop.API/Startup.cs
@@ -1,23 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
 // next 5 lines added in lab #2
 using BikeShop.API.Services;
 using BikeShop.API.Repositories;
 using BikeShop.API.Data;
 using Steeltoe.CloudFoundry.Connector.MySql;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
-
 //<debug only>
 using Steeltoe.CloudFoundry.Connector;
 using Steeltoe.CloudFoundry.Connector.Services;
@@ -29,7 +21,6 @@ namespace BikeShop.API
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
             // For debugging config/connection issues
             //var info = configuration.GetRequiredServiceInfo<MySqlServiceInfo>("mysql");
         }
@@ -40,12 +31,11 @@ namespace BikeShop.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
             // next 4 lines added in lab #2
             services.AddMySqlConnection(Configuration);
             services.AddTransient<BicycleService>();
             services.AddTransient<BicycleRepository>();
-            services.AddDbContext<BicycleDbContext>(o => o.UseMySql(Configuration, "mysql"));
+            services.AddDbContext<BicycleDbContext>(o => o.UseMySql(Configuration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
